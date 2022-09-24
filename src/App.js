1212621a6663331efe80a23d41816e7975c0d7be
SargisSuqiasyan <97920258+Basturman@users.noreply.github.com>
2022-09-24 15:55:32 +0400

    imageBagFixed

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Main from './main/Main'
 function App() {
 
   const [server,setServer]=useState([
-    {id:1,name:"Decor Plate",price:"$ 65.00 USD",img:"/imagesObj/image1.png"},
-    {id:2,name:"Mint Pottery",price:"$ 75.00 USD",img:"/imagesObj/image2.png"},
-    {id:3,name:"Set Of Potterys",price:"$ 125.00 USD",img:"/imagesObj/image3.png"},
-    {id:4,name:"Orange Ceramic",price:"$ 55.00 USD",img:"/imagesObj/image4.png"},
-    {id:5,name:"Dark Bowl",price:"$ 115.00 USD",img:"/imagesObj/image5.png"},
-    {id:6,name:"Square Pottery",price:"$ 75.00 USD",img:"/imagesObj/image6.png"}
+    {id:1,name:"Decor Plate",price:"$ 65.00 USD",img:require("./imagesObj/image1.png")},
+    {id:2,name:"Mint Pottery",price:"$ 75.00 USD",img:require("./imagesObj/image2.png")},
+    {id:3,name:"Set Of Potterys",price:"$ 125.00 USD",img:require("./imagesObj/image3.png")},
+    {id:4,name:"Orange Ceramic",price:"$ 55.00 USD",img:require("./imagesObj/image4.png")},
+    {id:5,name:"Dark Bowl",price:"$ 115.00 USD",img:require("./imagesObj/image5.png")},
+    {id:6,name:"Square Pottery",price:"$ 75.00 USD",img:require("./imagesObj/image6.png")}
   ])
-
+  
   return (
     <div className="App">
       <Header/>
